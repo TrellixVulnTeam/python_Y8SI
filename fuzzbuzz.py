8c8e7fbe591900e9f027fce5821ec63ae043d016
yoge-esh <yogesh124khanal@gmail.com>
2021-11-29 21:49:14 +0545

    fizzBuzz exercise

---
+++ fuzzbuzz.py
@@ -0,0 +1,20 @@
+def fizzBuzz(input):
+    if input %3 == 0 and input % 5 == 0:
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
+
+print(fizzBuzz(15))
+
+a = int(input('Enter a number: '))
+if a % 3 == 0 and a % 5 == 0:
+    print('FuxxBuzz')
+elif a % 3 == 0:
+    print('Fizz')
+elif a % 5 == 0:
+    print('Buzz')
+else:
+    print(a)
