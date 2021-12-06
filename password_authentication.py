0430bae0ce15ee9a6c3d005bc7efa0355d938f30
yoge-esh <yogesh124khanal@gmail.com>
2021-12-06 20:36:54 +0545

    password authentation

--- password_authentication.py
+++ password_authentication.py
@@ -1,15 +1,19 @@
 # password authentication 
-
-import getpass # importing model 
+import getpass # it dpesnot show how the password that we enter 
+import pwinput # when we enter a password it will comvert to astric 
+# importing model 
 database = {'yogesh':'123', 'khanal':'456'} # creating a dictionary and storing keys and value 
 username = input('Enter your name: ') # userinput for username 
-password = getpass.getpass('Enter your password: ') # userinput for password
-for i in database: # chicking (iterating) the database 
-    if username == i: #condition
-        print('user id is varified!') # True
-        while password != database.get(i): #condition 
-            password = getpass.getpass('Wrong password!.... Enter your passwod again: ') # False 
-        break 
+for i in database.keys():
+    if username == i:
+        # password = getpass.getpass("Enter your password: ")
+        password = pwinput.pwinput("Enter your password: ")
+        if password == database[i]:
+            print('Account is activated!')
+            break
+        else:
+            print("Password couldnot match!")
+            break
     else:
-        print('user id is not in database record!')
+        print('Username is not found!')
         break
