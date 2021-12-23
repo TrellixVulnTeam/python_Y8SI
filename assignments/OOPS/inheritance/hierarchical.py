bfa3d5afe97ad225553ebd186753d244556cbbea
yoge-esh <yogesh124khanal@gmail.com>
2021-12-23 18:03:32 +0545

    hierarchical inheritance

--- assignments/OOPS/inheritance/hierarchical.py
+++ assignments/OOPS/inheritance/hierarchical.py
@@ -29,3 +29,24 @@ print()
 print('====================>')
 object2.func1()
 object2.func3()
+
+
+
+# example of hirachicale inheritance 
+print()
+class base1:
+    def base1(self):
+        print('this is a base class 1')
+
+class base2(base1):
+    def base2(self):
+        print('this is a base class 2')
+
+class derive(base2):
+    def derive(self):
+        print('this is a derive class 1')
+
+obj = derive()
+obj.derive()
+obj.base2()
+obj.base1()
