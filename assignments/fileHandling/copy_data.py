6c54e8a73d15d196c56ada96bf0627f4bc16c75e
yoge-esh <yogesh124khanal@gmail.com>
2021-12-28 15:51:22 +0545

    copy data and with statement

---
+++ assignments/fileHandling/copy_data.py
@@ -0,0 +1,17 @@
+# copy of data 
+
+f1 = open('file.txt', mode = 'r')
+f2 = open('file3.txt', mode = 'w')
+data = f1.read()
+f2.write(data)
+f1.close()
+f2.close()
+
+
+# error 
+# with statement 
+with open('file.txt') as f:
+    data = f.read()
+    print(data)
+    print(f.closeed)
+print(f.closed)
