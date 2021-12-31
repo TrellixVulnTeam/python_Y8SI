0a65c3504ed6bf01eaa39dbc0415ec0f7ccb19e2
yoge-esh <yogesh124khanal@gmail.com>
2021-12-31 14:56:14 +0545

    random password generator

--- Projects/test.py
+++ Projects/test.py
@@ -5,18 +5,20 @@ import string
 
 def generate_Password(num):
     password = []
-    for i in range(num):
-        x = random.randint(0, 90)
+    for i in range(16):
+        x = random.randint(0, 94)
         password += string.printable[x]
-    return password
+    # return password
+    
+    lbl.config(text=password)
 
-print(generate_Password(16))
+# print(generate_Password(16))
 
 root = Tk()
 root.title('PASSWORD GENERETOR')
-root.geometry("100*100")
+root.geometry("100x100")
 btn = Button(root, font = ('times', 15, 'bold'))
 btn.grid(row = 2, column=2)
 lbl = Label(root, font=('tuime', 15, 'bold'))
-lbl.gird(row = 4, column = 2)
+lbl.grid(row = 4, column = 2)
 root.mainloop()
